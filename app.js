15898a9e396d5359a2ece875b14b5e54160cea9a
George Gardner <gpgardner@yahoo.com>
2020-04-19 20:33:53 -0400

    refactored

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
 // var requirejs = require('requirejs');
-$(document).ready($('#search').on('keyup', (event) => {
-  if(event.keyCode === 13){
-    getWeather();
-  }
-}));
-
-async function getWeather(){
-  let api_key = config.openWapiKey;
-    const zipCode = getZip();
-    try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${zipCode}&appid=${api_key}`);
-      const weather = await response.json();
-      console.log(weather);
-      display(weather);
-    } catch(err) {
-        alert('Please enter a valid Zip Code'); // TypeError: failed to fetch
+$(document).ready(
+  $("#search").on("keyup", (event) => {
+    if (event.keyCode === 13) {
+      getWeather();
     }
-  }
+  })
+);
 
-function display(weather){
-    const city = weather.name;
-    const temp = weather.main.temp;
-    const icon = weather.weather[0].icon;
-    const lat = weather.coord.lat;
-    const lon = weather.coord.lon;
-    
+async function getWeather() {
+  let api_key = config.openWapiKey;
+  const zipCode = getZip();
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${zipCode}&appid=${api_key}`
+    );
+    const weather = await response.json();
+    console.log(weather);
+    display(weather);
+  } catch (err) {
+    alert("Please enter a valid Zip Code"); // TypeError: failed to fetch
+  };
+};
 
-    $('#card').find('#city').text(city);
-    $('#temp').text(`${temp}° F`);
-    $('#weatherImg').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
-    getTimeZone(lat, lon)
-    
-}
+function display(weather) {
+  const city = weather.name;
+  const temp = weather.main.temp;
+  const icon = weather.weather[0].icon;
+  const lat = weather.coord.lat;
+  const lon = weather.coord.lon;
 
-async function getTimeZone(lat, lon){
+  $("#card").find("#city").text(city);
+  $("#temp").text(`${temp}° F`);
+  $("#weatherImg").attr(
+    "src",
+    `http://openweathermap.org/img/wn/${icon}@2x.png`
+  );
+  getTimeZone(lat, lon);
+  setInterval(() => {
+    getTimeZone(lat, lon);
+  }, 1000);
+};
+
+async function getTimeZone(lat, lon) {
   let apiKey = config.latapiKey;
-  const response = await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lon}`)
+  const response = await fetch(
+    `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lon}`
+  );
   const timeZone = await response.json();
-  console.log(timeZone)
-  getTime(timeZone);
-}
+  console.log(timeZone);
+  getTime(timeZone)
+};
 
-function getTime(timeZone){
-    // const moment = require('moment');
-    // const time = moment().format('h:mm:ss a');
-    let time = timeZone.formatted.slice(11,19);
-    
-    $('#time').text(`current local time: ${time}`);
-}
+function getTime(timeZone) {
+  // const moment = require('moment');
+  // const time = moment().format('h:mm:ss a');
+  let time = timeZone.formatted.slice(11, 19);
+  $("#time").text(`current local time: ${time}`);
+};
 
 function getZip() {
-    const zip = $('#search').val();
-    $('#search').val('');
-    return zip;
-}
+  const zip = $("#search").val();
+  $("#search").val("");
+  return zip;
+};
 
 // getWeather();
 //change the background to a specific img for the weather type
